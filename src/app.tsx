2e2d97d5f038cb0edac10d2fd69cd7fcb424b181
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-08-04 17:04:27 +0200

    Finish authentication

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
-import { HomePage, LoginPage } from './ui/pages';
+import { HomePage, LoginPage, SignUpPage } from './ui/pages';
 
 import history from './utils/history';
-import { ROUTE_HOME, ROUTE_LOGIN } from './utils';
+import { ROUTE_HOME, ROUTE_LOGIN, ROUTE_SIGN_UP } from './utils';
 
 export class App extends React.Component<any, any> {
     render() {
@@ -15,6 +15,7 @@ export class App extends React.Component<any, any> {
                     <Switch>
                         <Route exact path={ROUTE_HOME} component={HomePage} />
                         <Route exact path={ROUTE_LOGIN} component={LoginPage} />
+                        <Route exact path={ROUTE_SIGN_UP} component={SignUpPage} />
                     </Switch>
                 </Router>
             </MuiThemeProvider>
